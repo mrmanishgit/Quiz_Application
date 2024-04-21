25607d18ff49497eabd0bc9108f216172d65c47a
mrmanishgit <MAHARANAMANISH91@GMAIL.com>
2024-04-21 21:22:47 +0530

    project

---
+++ src/quiz/application/Quiz.java
@@ -0,0 +1,313 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Quiz extends JFrame implements ActionListener {
+    
+    String questions[][] = new String[10][5];
+    String answers[][] = new String[10][2];
+    String useranswers[][] = new String[10][1];
+    JLabel qno, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+    ButtonGroup groupoptions;
+    JButton next, submit, lifeline;
+    
+    public static int timer = 15;
+    public static int ans_given = 0;
+    public static int count = 0;
+    public static int score = 0;
+    
+    String name;
+    
+    Quiz(String name) {
+        this.name = name;
+        setBounds(50, 0, 1440, 850);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1440, 392);
+        add(image);
+        
+        qno = new JLabel();
+        qno.setBounds(100, 450, 50, 30);
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(qno);
+        
+        question = new JLabel();
+        question.setBounds(150, 450, 900, 30);
+        question.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(question);
+        
+        questions[0][0] = "What is the process of putting money into your bank account called?";
+        questions[0][1] = "Investment";
+        questions[0][2] = "Deposit";
+        questions[0][3] = "Transfer";
+        questions[0][4] = "Loan";
+
+        questions[1][0] = "Which of the following options is NOT a way to withdraw money from your account?";
+        questions[1][1] = "Using an ATM";
+        questions[1][2] = "Online shopping";
+        questions[1][3] = "Direct deposit";
+        questions[1][4] = "Bank teller";
+
+        questions[2][0] = "If you want to check your bank account balance, which of the following methods can you NOT use?";
+        questions[2][1] = "Online banking";
+        questions[2][2] = "ATM";
+        questions[2][3] = "Writing a check";
+        questions[2][4] = "Mobile app";
+
+        questions[3][0] = "What is the maximum daily withdrawal limit from an ATM typically?";
+        questions[3][1] = "$100-$200";
+        questions[3][2] = "$200-$400";
+        questions[3][3] = "$500-$1000";
+        questions[3][4] = "$1000-$2000";
+
+        questions[4][0] = "Which feature would you typically use to exit an online banking session?";
+        questions[4][1] = "Log out";
+        questions[4][2] = "Close the browser window";
+        questions[4][3] = "Turn off your computer";
+        questions[4][4] = "All of the above";
+
+        questions[5][0] = "Which of the following is a secure way to perform online banking transactions?";
+        questions[5][1] = "Using public Wi-Fi";
+        questions[5][2] = "Using a VPN";
+        questions[5][3] = "Using an insecure browser";
+        questions[5][4] = "All of the above";
+
+        questions[6][0] = "What should you do if you find an unauthorized transaction on your account?";
+        questions[6][1] = "Ignore it";
+        questions[6][2] = " Report it to the bank immediately";
+        questions[6][3] = "Ask a friend what to do";
+        questions[6][4] = "Close your account";
+
+        questions[7][0] = "What is a required step before you can withdraw cash from an ATM?";
+        questions[7][1] = "Insert your credit card";
+        questions[7][2] = "Provide a fingerprint";
+        questions[7][3] = "Insert your debit card and enter PIN";
+        questions[7][4] = "Ask the bank teller for permission";
+
+        questions[8][0] = "How can you deposit a check into your bank account?";
+        questions[8][1] = "By mailing it to the bank";
+        questions[8][2] = "Through an ATM that accepts deposits";
+        questions[8][3] = "Using a mobile deposit via the bank’s app";
+        questions[8][4] = "All of the above";
+
+        questions[9][0] = "How can you confirm a bank transaction has been successful?";
+        questions[9][1] = "You receive a confirmation message or email";
+        questions[9][2] = "The balance updates in your account";
+        questions[9][3] = "You hear from a friend that it was successful";
+        questions[9][4] = "A and B";
+        
+        answers[0][1] = "Deposit";
+        answers[1][1] = "Direct deposit";
+        answers[2][1] = "Writing a check";
+        answers[3][1] = "$500-$1000";
+        answers[4][1] = "Log out";
+        answers[5][1] = "Using a VPN";
+        answers[6][1] = "Report it to the bank immediately";
+        answers[7][1] = "Insert your debit card and enter PIN";
+        answers[8][1] = "All of the above";
+        answers[9][1] = "A and B";
+        
+        opt1 = new JRadioButton();
+        opt1.setBounds(170, 520, 700, 30);
+        opt1.setBackground(Color.WHITE);
+        opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt1);
+        
+        opt2 = new JRadioButton();
+        opt2.setBounds(170, 560, 700, 30);
+        opt2.setBackground(Color.WHITE);
+        opt2.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt2);
+        
+        opt3 = new JRadioButton();
+        opt3.setBounds(170, 600, 700, 30);
+        opt3.setBackground(Color.WHITE);
+        opt3.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt3);
+        
+        opt4 = new JRadioButton();
+        opt4.setBounds(170, 640, 700, 30);
+        opt4.setBackground(Color.WHITE);
+        opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt4);
+        
+        groupoptions = new ButtonGroup();
+        groupoptions.add(opt1);
+        groupoptions.add(opt2);
+        groupoptions.add(opt3);
+        groupoptions.add(opt4);
+        
+        next = new JButton("Next");
+        next.setBounds(1100, 550, 200, 40);
+        next.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        next.setBackground(new Color(30, 144, 255));
+        next.setForeground(Color.WHITE);
+        next.addActionListener(this);
+        add(next);
+        
+        lifeline = new JButton("50-50 Lifeline");
+        lifeline.setBounds(1100, 630, 200, 40);
+        lifeline.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        lifeline.setBackground(new Color(30, 144, 255));
+        lifeline.setForeground(Color.WHITE);
+        lifeline.addActionListener(this);
+        add(lifeline);
+        
+        submit = new JButton("Submit");
+        submit.setBounds(1100, 710, 200, 40);
+        submit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        submit.setEnabled(false);
+        add(submit);
+        
+        start(count);
+        
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
+            repaint();
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+               useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            
+            count++;
+            start(count);
+        } else if (ae.getSource() == lifeline) {
+            if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9) {
+                opt2.setEnabled(false);
+                opt3.setEnabled(false);
+            } else {
+                opt1.setEnabled(false);
+                opt4.setEnabled(false);
+            }
+            lifeline.setEnabled(false);
+        } else if (ae.getSource() == submit) {
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+
+            for (int i = 0; i < useranswers.length; i++) {
+                if (useranswers[i][0].equals(answers[i][1])) {
+                    score += 10;
+                } else {
+                    score += 0;
+                }
+            }
+            setVisible(false);
+            new Score(name, score);
+        }
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        String time = "Time left - " + timer + " seconds"; // 15
+        g.setColor(Color.RED);
+        g.setFont(new Font("Tahoma", Font.BOLD, 25));
+        
+        if (timer > 0) { 
+            g.drawString(time, 1100, 500);
+        } else {
+            g.drawString("Times up!!", 1100, 500);
+        }
+        
+        timer--; // 14
+        
+        try {
+            Thread.sleep(1000);
+            repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        if (ans_given == 1) {
+            ans_given = 0;
+            timer = 15;
+        } else if (timer < 0) {
+            timer = 15;
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            if (count == 9) { // submit button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                
+                for (int i = 0; i < useranswers.length; i++) {
+                    if (useranswers[i][0].equals(answers[i][1])) {
+                        score += 10;
+                    } else {
+                        score += 0;
+                    }
+                }
+                setVisible(false);
+                new Score(name, score);
+            } else { // next button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                count++; // 0 // 1
+                start(count);
+            }
+        }
+        
+    }
+    
+    public void start(int count) {
+        qno.setText("" + (count + 1) + ". ");
+        question.setText(questions[count][0]);
+        opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);
+        
+        opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]);
+        
+        opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+        
+        opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+        
+        groupoptions.clearSelection();
+    }
+    
+    public static void main(String[] args) {
+        new Quiz("User");
+    }
+}
